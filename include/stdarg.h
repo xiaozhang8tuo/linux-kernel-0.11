4b2aee6126d740e7e706726bb2f6ccf668a5e98b
zyx <874279750@qq.com>
2023-02-03 19:53:19 -0800

    main.c stdarg.h:printf怎么实现的可变参数输出 va_start va_arg va_list

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,14 +1,29 @@
 #ifndef _STDARG_H
 #define _STDARG_H
 
-typedef char *va_list;
+typedef char *va_list;  //定义va_list是一个字符指针类型
 
 /* Amount of space required in an argument list for an arg of type TYPE.
-   TYPE may alternatively be an expression whose type is used.  */
+   TYPE may alternatively be an expression whose type is used.  
+  下面给出了类型为type的arg参数列表所要求的空间容量, type也可以是使用该类型的一个表达式   
+*/
 
+// 定义了取整后的type类型的字节长度值,是int长度(4)的整数倍
 #define __va_rounded_size(TYPE)  \
   (((sizeof (TYPE) + sizeof (int) - 1) / sizeof (int)) * sizeof (int))
 
+
+// 下面这个宏初始化指针AP,使其指向传给函数的可变参数表的第一个参数。
+// 在第一次调用va_arg或va_end之前，必须首先调用var_start宏。参数LASTARG是函数定义
+// 中最右边参数的标识符，即'...’左边的一个标识符。AP是可变参数表参数指针，LASTARG是
+// 最后一个指定的参数。&(LASTARG)用于取其地址（即其指针），并且该指针是字符类型。加上
+// LASTARG的宽度值后AP就是可变参数表中第一个参数的指针。该宏没有返回值。
+// 第17行上的函数__builtin_saveregs是在gcc的库程序1 ibgcc2.c中定义的，用于保存
+// 寄存器。相关说明参见gcc手册“Target Description Macros”章中“Implementing the
+// /arargs Macros”小节。
+// 用法: printf(const char *fmt, ...);   va_list args;   va_start(args, fmt);
+
+//https://www.runoob.com/cprogramming/c-variable-arguments.html 
 #ifndef __sparc__
 #define va_start(AP, LASTARG) 						\
  (AP = ((char *) &(LASTARG) + __va_rounded_size (LASTARG)))
@@ -18,11 +33,37 @@ typedef char *va_list;
   AP = ((char *) &(LASTARG) + __va_rounded_size (LASTARG)))
 #endif
 
+// 下面该宏用于被调用函数完成一次正常返回。va_end可以修改AP使其在重新调用
+// va_start之前不能被使用。va_end必须在va_arg读完所有的参数后再被调用。
 void va_end (va_list);		/* Defined in gnulib */
 #define va_end(AP)
 
+// 下面宏用于扩展表达式使其与下一个被传递参数具有相同的类型和值。
+// va_arg宏会扩展成函数参数列表中下一个参数的类型和值。AP应该与va_start初始化的
+// valist AP相同。每次调用va_arg时都会修改AP,使得下一个参数的值被返回。TYPE是
+// 一个类型名。
+// 在va_start初始化之后，第1次调用va_arg会返回LASTARG指定参数后的
+// 参数值。随后的调用会返回随后参数的值。
+//
+// 实现即AP++,指向下一个参数。然后返回AP之前指向的参数的值
 #define va_arg(AP, TYPE)						\
  (AP += __va_rounded_size (TYPE),					\
-  *((TYPE *) (AP - __va_rounded_size (TYPE))))
+  *((TYPE *) (AP - __va_rounded_size (TYPE))))   //宏函数的返回值，即宏函数中最后一个表达式的值；宏函数的返回值的类型，是最后一个表达式的类型。
 
 #endif /* _STDARG_H */
+
+
+
+/*
+printf("test:%d,%d", a, b)
+
+入栈时的栈空间: 1:从右向左依次入栈 2:字节对齐
+3----0(32位)
+
+b
+a         va_start      <---- AP
+%d
+:%d,
+test      <----LASTARG  __va_rounded_size(LASTARG) = 3,  AP(arg pointer) = LASTARG+3即指向了第一个可变参数
+
+*/
--- init/main.c
+++ init/main.c
@@ -5,29 +5,29 @@
  */
 // 定义宏"__LIBRARY__" 是为了包括定义在unistd.h中的内嵌汇编代码信息
 #define __LIBRARY__
-// *.h头文件所在的默认目录是include/,则在代码中就不用明确指明其位置。如果不是UNIX的
-// 标准头文件，则需要指明所在的目录，并用双引号括住。unistd.h是标准符号常数与类型文件。
-// 其中定义了各种符号常数和类型，并声明了各种函数。如果还定义了符号LIBRARY,则还会
-// 包含系统调用号和内嵌汇编代码syscal100等。
+// *.h头文件所在的默认目录是include/,则在代码中就不用明确指明其位置,如果不是UNIX的
+// 标准头文件，则需要指明所在的目录，并用双引号括住,unistd.h是标准符号常数与类型文件,
+// 其中定义了各种符号常数和类型，并声明了各种函数,如果还定义了符号LIBRARY,则还会
+// 包含系统调用号和内嵌汇编代码syscal100等,
 
 #include <unistd.h>
 #include <time.h>   // 时间类型头文件,主要定义了tm结构和一些有关时间的函数原型
 
 /*
 	我们需要下面这些内嵌语句,从内核空间创建进程将导致没有写时复制(COPY ON WRITE)!!
-直到执行一个execve调用。这对堆栈可能带来问题。处理方法是在fork调用后不让main
-使用任何堆栈。因此就不能有函数调用,这意味着fork也要使用内嵌的代码,否则我们在从
+直到执行一个execve调用,这对堆栈可能带来问题,处理方法是在fork调用后不让main
+使用任何堆栈,因此就不能有函数调用,这意味着fork也要使用内嵌的代码,否则我们在从
 fork退出时就要使用堆栈了
 	实际上只有pause和fork需要使用内嵌方式,以保证从mian中不会弄乱堆栈.但还是定义了其他的一些函数
  */
-// Linux在内核空间创建进程时不使用写时复制技术(Copy on write)。main()在移动到用户
-// 模式（到任务0）后执行内嵌方式的fork和pause,因此可保证不使用任务0的用户栈。
-// 在执行move_to_user_mode之后，本程序main()就以任务0的身份在运行了。而任务0是所
-// 有将创建子进程的父进程。当它创建一个子进程时(init进程)，由于任务1代码属于内核
-// 空间，因此没有使用写时复制功能。此时任务0的用户栈就是任务1的用户栈，即它们共同
-// 使用一个栈空间。因此希望在main.c运行在任务0的环境下时不要有对堆栈的任何操作，以
-// 免弄乱堆栈。而在再次执行fork()并执行过execve()函数后，被加载程序已不属于内核空间，
-// 因此可以使用写时复制技术了。参见上一章“Liux内核使用内存的方法”一节内容。
+// Linux在内核空间创建进程时不使用写时复制技术(Copy on write),main()在移动到用户
+// 模式(到任务0)后执行内嵌方式的fork和pause,因此可保证不使用任务0的用户栈,
+// 在执行move_to_user_mode之后，本程序main()就以任务0的身份在运行了,而任务0是所
+// 有将创建子进程的父进程,当它创建一个子进程时(init进程)，由于任务1代码属于内核
+// 空间，因此没有使用写时复制功能,此时任务0的用户栈就是任务1的用户栈，即它们共同
+// 使用一个栈空间,因此希望在main.c运行在任务0的环境下时不要有对堆栈的任何操作，以
+// 免弄乱堆栈,而在再次执行fork()并执行过execve()函数后，被加载程序已不属于内核空间，
+// 因此可以使用写时复制技术了,参见上一章“Liux内核使用内存的方法”一节内容,
 
 
 static inline _syscall0(int,fork)
@@ -46,22 +46,22 @@ static inline _syscall0(int,pause)
 static inline _syscall1(int,setup,void *,BIOS)  //setup(void* BIOS)系统调用，仅用于linux初始化(仅在这个程序中调用)
 static inline _syscall0(int,sync)               //系统调用,更新文件系统
 
-#include <linux/tty.h>      //tty头文件，定义了有关tty_io,串行通信方面的参数、常数。
+#include <linux/tty.h>      //tty头文件，定义了有关tty_io,串行通信方面的参数、常数,
 #include <linux/sched.h>	//调度程序头文件，定义了任务结构task_struct、第1个初始任务
-							//的数据。还有一些以宏的形式定义的有关描述符参数设置和获取的
-							//嵌入式汇编函数程序。
+							//的数据,还有一些以宏的形式定义的有关描述符参数设置和获取的
+							//嵌入式汇编函数程序,
 #include <linux/head.h>     //head头文件,定义了段描述符的简单结构,和价格选择符常量
 #include <asm/system.h>     //系统头文件,以宏的形式定义了许多有关设置或修改 描述符/中断门等嵌入式汇编子程序
 #include <asm/io.h>         //io头文件,以宏的嵌入式汇编程序形式定义对io端口操作的函数
 
-#include <stddef.h>         //标准定义头文件。定义了NULL,offsetof(TYPE,MEMBER)。
-#include <stdarg.h>			//标准参数头文件。以宏的形式定义变量参数列表。主要说明了以个
+#include <stddef.h>         //标准定义头文件,定义了NULL,offsetof(TYPE,MEMBER),
+#include <stdarg.h>			//标准参数头文件,以宏的形式定义变量参数列表,主要说明了以个
 							//类型(va_list)和三个宏(va_start,va_arg和va_end),vsprintf、vprintf,vfpringf
 #include <unistd.h>
-#include <fcntl.h>			//文件控制头文件。用于文件及其描述符的操作控制常数符号的定义。
-#include <sys/types.h>		//类型头文件。定义了基本的系统数据类型。
+#include <fcntl.h>			//文件控制头文件,用于文件及其描述符的操作控制常数符号的定义,
+#include <sys/types.h>		//类型头文件,定义了基本的系统数据类型,
 
-#include <linux/fs.h>		//文件系统头文件。定义文件表结构(file,buffer_head,m_inode等)。
+#include <linux/fs.h>		//文件系统头文件,定义文件表结构(file,buffer_head,m_inode等),
 
 static char printbuf[1024]; 				//静态字符数组，用作内核显示信息的缓存
 
@@ -79,10 +79,10 @@ extern long startup_time;							//内核启动时间(开机时间)
 /*
  * This is set up by the setup-routine at boot-time 这些数据是在boot期间由setup.s程序设置的
  */
-// 下面三行分别将指定的线性地址强行转换为给定数据类型的指针，并获取指针所指内容。由于
-// 内核代码段被映射到从物理地址零开始的地方，因此这些线性地址正好也是对应的物理地址。
-// 这些指定地址处内存值的含义请参见第6章的表6-2(setup程序读取并保存的参数)。
-// drive_info结构请参见下面第1O2行。
+// 下面三行分别将指定的线性地址强行转换为给定数据类型的指针，并获取指针所指内容,由于
+// 内核代码段被映射到从物理地址零开始的地方，因此这些线性地址正好也是对应的物理地址,
+// 这些指定地址处内存值的含义请参见第6章的表6-2(setup程序读取并保存的参数),
+// drive_info结构请参见下面第1O2行,
 #define EXT_MEM_K (*(unsigned short *)0x90002)        //扩展内存数 系统从1MB开始的扩展内存数值(KB)
 #define DRIVE_INFO (*(struct drive_info *)0x90080)	  //硬盘参数表,第一个硬盘的参数表
 #define ORIG_ROOT_DEV (*(unsigned short *)0x901FC)	  //根设备号: 根文件系统所在的设备号(bootsect.s中设置)
@@ -95,14 +95,14 @@ extern long startup_time;							//内核启动时间(开机时间)
  * bios-listing reading. Urghh.
  */
 
-//这段宏读取COS实时时钟信息。outb_p和inb_p是include/asm/io.h中定义的端口输入输出宏。
+//这段宏读取COS实时时钟信息,outb_p和inb_p是include/asm/io.h中定义的端口输入输出宏,
 #define CMOS_READ(addr) ({ \
 outb_p(0x80|addr,0x70); \       //0x70是写地址端口,0x80|addr是要读取的CMOS内存地址
 inb_p(0x71); \					//0x71是读数据端口
 })
 
 //定义宏，将BCD码转换成二进制数值, BCD码利用半个字节(4bit)表示一个10进制数，因此
-//1个字节表示2个10进制数. val&15(1111)取BCD的低四位表示的10进制个位数,而val>>4取BCD表示的10进制十位数,再*10。最后两者相加
+//1个字节表示2个10进制数. val&15(1111)取BCD的低四位表示的10进制个位数,而val>>4取BCD表示的10进制十位数,再*10,最后两者相加
 //即一个字节BCD码的实际二进制数值
 #define BCD_TO_BIN(val) ((val)=((val)&15) + ((val)>>4)*10)
 
@@ -114,7 +114,7 @@ static void time_init(void)
 	struct tm time;                          //时间结构tm定义在time.h中
 
 
-	// CMOS的访问速度慢，为了减小时间误差，在读取下面所有值之后，如果秒值发生变化，就重新读取。把与CMOS的时差控制在1s
+	// CMOS的访问速度慢，为了减小时间误差，在读取下面所有值之后，如果秒值发生变化，就重新读取,把与CMOS的时差控制在1s
 	do {
 		time.tm_sec = CMOS_READ(0);          //当前时间秒值
 		time.tm_min = CMOS_READ(2);			 //当前时间分钟值
@@ -167,7 +167,7 @@ void main(void)		/* This really IS void, no error here. */
 #ifdef RAMDISK
 	main_memory_start += rd_init(main_memory_start, RAMDISK*1024);
 #endif
-//以下是内核进行所有方面的初始化工作。
+//以下是内核进行所有方面的初始化工作,
 	mem_init(main_memory_start,memory_end); 		//主内存区初始化(mm/memory.c)
 	trap_init();									//陷阱门(硬件中断向量)初始化 (traps.c)
 	blk_dev_init();//块设备							
@@ -186,15 +186,22 @@ void main(void)		/* This really IS void, no error here. */
 		init();			//子进程中执行
 	}
 /*
- *   NOTE!!   For any other task 'pause()' would mean we have to get a
- * signal to awaken, but task0 is the sole exception (see 'schedule()')
- * as task 0 gets activated at every idle moment (when no other tasks
- * can run). For task0 'pause()' just means we go check if some other
- * task can run, and if not we return here.
+	注意！！对于任何其他的任务，’pause()'将意味着我们必须等待收到一个信号
+才会返回就绪态，但任务0(task0)是唯一例外情况(参见'schedule()'),
+因为任务0在任何空闲时间里都会被微活(当没有其他任务在运行时)，因此
+对于任务0'pause()'仅意味着我们返回来查看是否有其他任务可以运行，如果
+没有的话我们就回到这里，一直循环执行'pause()’,
+	pause()系统调用(kernel/sched.c)会把任务0转换成可中断等待状态，再执行调度函数
+但是调度函数只要发现系统中没有其他任务可以运行时就会切换到任务0，而不依赖于任务0的
+状态,
  */
 	for(;;) pause();
 }
 
+// 下面函数产生格式化信息并输出到标准输出设备stdout(1),这里是指屏幕上显示,参数'*mt
+// 指定输出将采用的格式，参见标准C语言书籍,该子程序正好是vsprintf如何使用的一个简单
+// 列子,该程序使用vsprintf()将格式化的字符串放入printbuf缓冲区，然后用write将缓冲
+// 区的内容输出到标准设备(I-stdout),vsprintf()函数的实现见kernel/vsprintf.c,
 static int printf(const char *fmt, ...)
 {
 	va_list args;
@@ -206,33 +213,77 @@ static int printf(const char *fmt, ...)
 	return i;
 }
 
-static char * argv_rc[] = { "/bin/sh", NULL };
-static char * envp_rc[] = { "HOME=/", NULL };
+//读取并执行/etc/rc文件时所使用的命令行参数和环境参数,
+static char * argv_rc[] = { "/bin/sh", NULL };   //调用执行程序时参数的字符串数组
+static char * envp_rc[] = { "HOME=/", NULL };    //调用执行程序时的环境字符串数组
 
+
+//运行登录shell时所使用的命令行参数和环境参数
+//argv[0]中的字符“-”是传递给shell程序sh的一个标志,通过识别该标志，
+//sh程序会作为登录shell执行,其执行过程与在shell提示符下执行sh不一样,
 static char * argv[] = { "-/bin/sh",NULL };
 static char * envp[] = { "HOME=/usr/root", NULL };
 
+
+// 在main()中已经进行了系统初始化，包括内存管理、各种硬件设备和驱动程序,init()函数
+// 运行在任务0第1次创建的子进程(任务1)中,它首先对第一个将要执行的程序(shell)
+// 的环境进行初始化，然后以登录shell方式加载该程序并执行之,
 void init(void)
 {
 	int pid,i;
 
+// setup()是一个系统调用,用于读取硬盘参数包括分区表信息并加截虚拟盘(若存在的话)和
+// 安装根文件系统设备,该函数用46行上的宏定义，对应函数是sys_setup(),在块设备子目录
+// kernel/blk_drv/hd.c行,
 	setup((void *) &drive_info);
+
+// 下面以读写访间方式打开设备“/dev/tty0”,它对应终瑞控制台,由于这是第一次打开文件
+// 操作，因此产生的文件句柄号(文件描述符)肯定是0,该句柄是UNIX类操作系统默认的控
+// 制台标准输入句柄stdin,这里再把它以读和写的方式分别打开是为了复制产生标准输出(写)
+// 句柄stdout和标准出错输出句柄stderr,
+// 函数前面的“(void)”前缀用于表示强制函数无需返回值,
 	(void) open("/dev/tty0",O_RDWR,0);
-	(void) dup(0);
-	(void) dup(0);
+	(void) dup(0);     //复制句柄,产生句柄1号--stdout标准输出设备
+	(void) dup(0);	   //复制句柄,产生句柄2号--stderr标准出错设备
+// 下面打印缓冲区块数和总字节数，每块1024字节，以及主内存区空闲内存字节数,
 	printf("%d buffers = %d bytes buffer space\n\r",NR_BUFFERS,
 		NR_BUFFERS*BLOCK_SIZE);
 	printf("Free mem: %d bytes\n\r",memory_end-main_memory_start);
+
+// 下面fork()用于创建一个子进程(任务2),对于被创建的子进程，fork()将返回0值，对于
+// 原进程(父进程)则返回子进程的进程号pid,该子
+// 进程关闭了句柄0(stdin)、以只读方式打开/etc/rc文件，并使用execve()函数将进程自身
+// 替换成bin/sh程序(即shell程序)，然后执行bin/sh程序,所携带的参数和环境变量分
+// 别由argv_rc和envp_rc数组给出,关闭句柄0并立刻打开/etc/rc文件的作用是把标准输入
+// stdin重定向到/etc/rc文件,这样shell程序bin/sh就可以运行rc文件中设置的命令,由
+// 于这里sh的运行方式是非交互式的，因此在执行完rc文件中的命令后就会立刻退出，进程2
+// 也随之结束,关于execve()函数说明请参见fs/exec.c程序,
+// 函数exit()退出时的出错码1-操作未许可：2-文件或目录不存在,
 	if (!(pid=fork())) {
 		close(0);
-		if (open("/etc/rc",O_RDONLY,0))
-			_exit(1);
-		execve("/bin/sh",argv_rc,envp_rc);
-		_exit(2);
+		if (open("/etc/rc",O_RDONLY,0))  //把标准输入stdin重定向到/etc/rc中
+			_exit(1);                    //如果打开文件失败，则退出(1ib/_exit.c,10)
+		execve("/bin/sh",argv_rc,envp_rc); 
+		_exit(2);                        //若execve()执行失败则退出
 	}
+
+// 下面还是父进程(1)执行的语句,wait()等待子进程停止或终止，返回值应是子进程的进程号
+// (pid),这三句的作用是父进程等待子进程的结束,&i是存放返回状态信息的位置,如果wait()
+// 返回值不等于子进程号，则继续等待,
 	if (pid>0)
 		while (pid != wait(&i))
 			/* nothing */;
+
+// 如果执行到这里，说明刚创建的子进程的执行已停止或终止了,下面循环中首先再创建一个子
+// 进程，如果出错，则显示“初始化程序创建子进程失败”信息并继续执行,对于所创建的子进
+// 程将关闭所有以前还遗留的句柄(stdin,stdout,stderr),新创建一个会话并设置进程组号，
+// 然后重新打开/dev/tty0作为stdin,并复制成stdout和stderr,再次执行系统解释程序
+// /bin/sh,但这次执行所选用的参数和环境数组另选了一套(见上面165-167行),然后父进
+// 程再次运行wait()等待,如果子进程又停止了执行，则在标准输出上显示出错信息“子进程
+// pid停止了运行，返回码是i”,然后继续重试下去，形成一个“大”循环,此外，wait()
+// 的另外一个功能是处理孤儿进程,如果一个进程的父进程先终止了，那么这个进程的父进程
+// 就会被设置为这里的init进程(进程1)，并由init进程负责释放一个已终止进程的任务数
+// 据结构等资源,
 	while (1) {
 		if ((pid=fork())<0) {
 			printf("Fork failed in init\r\n");
@@ -240,7 +291,7 @@ void init(void)
 		}
 		if (!pid) {
 			close(0);close(1);close(2);
-			setsid();
+			setsid();							//创建新的会话期
 			(void) open("/dev/tty0",O_RDWR,0);
 			(void) dup(0);
 			(void) dup(0);
@@ -253,4 +304,7 @@ void init(void)
 		sync();
 	}
 	_exit(0);	/* NOTE! _exit, not exit() */
+// _exit()和exit()都用于正常终止一个函数,但_exit直接是一个sys_exit系统调用，而
+// exit()则通常是普通函数库中的一个函数,它会先执行一些清除操作，例如调用执行各终止
+// 处理程序、关闭所有标准I0等，然后调用sys_exit,
 }
