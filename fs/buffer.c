e079e58aef7f468ac072c4d4e0d4d27e03d439c8
zyx <874279750@qq.com>
2023-02-14 08:43:06 -0800

    buffer.c: free_list&hash_map init

--- fs/buffer.c
+++ fs/buffer.c
@@ -427,6 +427,11 @@ void bread_page(unsigned long address,int dev,int b[4])
 	struct buffer_head * bh[4];
 	int i;
 
+	// 该函数循环执行4次，根据放在数组b[]中的4个块号从设备dev中读取一页内容放到指定
+	// 内存位置address处。对于参数b[i]给出的有效块号，函数首先从高速缓冲中取指定设备
+	// 和块号的缓冲块。如果缓冲块中数据无效（未更新）则产生读设备请求从设备上读取相应数
+	// 据块。对于b[i]无效的块号则不用去理它了。因此本函数其实可以根据指定的b[]中的块号
+	// 随意读取1一4个数据块。
 	for (i=0 ; i<4 ; i++)
 		if (b[i]) {
 			if (bh[i] = getblk(dev,b[i]))
@@ -435,7 +440,9 @@ void bread_page(unsigned long address,int dev,int b[4])
 		} else
 			bh[i] = NULL;
 
-	
+	// 随后将4个缓冲块上的内容顶序复制到指定地址处。在进行复削（使用）缓冲块之前我们
+	// 先要睡眠等待缓冲块解锁（若被上锁的话）。另外，因为可能睡眠过了，所以我们还需要
+	// 在复制之前再检查一下缓冲块中的数据是否是有效的。复制完后我们还需要释放缓冲块。
 	for (i=0 ; i<4 ; i++,address += BLOCK_SIZE)
 		if (bh[i]) {
 			wait_on_buffer(bh[i]);
@@ -450,24 +457,36 @@ void bread_page(unsigned long address,int dev,int b[4])
  * blocks for reading as well. End the argument list with a negative
  * number.
  */
+
+// 从指定设备读取指定的一些块。函数参数个数可变，是一系列指定的块号。成功时返回第1块的缓冲块头指针，否则返回NULL
 struct buffer_head * breada(int dev,int first, ...)
 {
 	va_list args;
 	struct buffer_head * bh, *tmp;
 
+	// 首先取可变参数表中第1个参数（块号）。接着从高速缓冲区中取指定设备和块号的缓冲
+	// 块。如果该缓冲块数据无效（更新标志未置位），则发出读设备数据块请求。
 	va_start(args,first);
 	if (!(bh=getblk(dev,first)))
 		panic("bread: getblk returned NULL\n");
 	if (!bh->b_uptodate)
 		ll_rw_block(READ,bh);
+	
+	// 然后顺序取可变参数表中其他预读块号，并作与上面同样处理，但不引用。注意，483行上
+	// 有一个bug。其中的bh应该是tmp。这个bug直到在0.96版的内核代码中才被纠正过来。
+	// 因为这里是预读随后的数据块，只需读进高速缓冲区但并不是马上就使用，所以第484行语
+	// 句需要将其引用计数递减释放掉该块（因为getblk函数会增加引用计数值）。
 	while ((first=va_arg(args,int))>=0) {
 		tmp=getblk(dev,first);
 		if (tmp) {
 			if (!tmp->b_uptodate)
 				ll_rw_block(READA,bh);
-			tmp->b_count--;
+			tmp->b_count--;					//暂时释放该预读块
 		}
 	}
+
+	// 此时可变参数表中所有参数处理完毕。于是等待第1个缓冲区解锁（如果已被上锁）。在等
+	// 待退出之后如果缓冲区中数据仍然有效，则返回缓冲区头指针退出。否则释放该缓冲区返回NULL,退出。
 	va_end(args);
 	wait_on_buffer(bh);
 	if (bh->b_uptodate)
@@ -476,37 +495,62 @@ struct buffer_head * breada(int dev,int first, ...)
 	return (NULL);
 }
 
+// 缓冲区初始化函数。
+// 参数buffer_end是缓冲区内存末端。对于具有16MB内存的系统，缓冲区末瑞被设置为4MB.
+// 对于有8MB内存的系统，缓冲区末瑞被设置为2MB。该函数从缓冲区开始位置start_buffer
+// 处和缓冲区未端buffer_end处分别同时设置（初始化）缓冲块头结构和对应的数据块。直到
+// 缓冲区中所有内存被分配完毕。参见程序列表前面的示意图。
 void buffer_init(long buffer_end)
 {
 	struct buffer_head * h = start_buffer;
 	void * b;
 	int i;
 
+	// 首先根据参数提供的缓冲区高端位置确定实际缓冲区高端位置b。如果缓冲区高端等于1Mb,
+	// 则因为从640KB一1MB被显示内存和BIOS占用，所以实际可用缓冲区内存高端位置应该是
+	// 640KB。否则缓冲区内存高端一定大于1MB。
 	if (buffer_end == 1<<20)
 		b = (void *) (640*1024);
 	else
 		b = (void *) buffer_end;
-	while ( (b -= BLOCK_SIZE) >= ((void *) (h+1)) ) {
-		h->b_dev = 0;
-		h->b_dirt = 0;
-		h->b_count = 0;
-		h->b_lock = 0;
-		h->b_uptodate = 0;
-		h->b_wait = NULL;
-		h->b_next = NULL;
-		h->b_prev = NULL;
-		h->b_data = (char *) b;
-		h->b_prev_free = h-1;
-		h->b_next_free = h+1;
+	
+	// 这段代码用于初始化缓冲区，建立空闲缓冲块循环链表，并获取系统中缓冲块数目。操作的
+	// 过程是从缓冲区高瑞开始划分1KB大小的缓冲块，与此同时在缓冲区低端建立描述该缓冲块
+	// 的结构buffer_head,并将这些buffer_head组成双向链表，
+	// h是指向缓冲头结构的指针，而h+1是指向内存地址连续的下一个缓冲头地址，可以说是
+	// 指向h缓冲头的末瑞外。为了保证有足够长度的内存来存储一个缓冲头结构，需要b所指向
+	// 的内存块地址>=h缓冲头的末端，即要求>=h+1。
+	// ------------------------------------------------------
+	// ↑			 (640KB)↑--------↑(1MB)					↑
+	// start_buffer         BIOS + 显存			   buffer_end
+	while ( (b -= BLOCK_SIZE) >= ((void *) (h+1)) ) {		// 尽可能多的分配直到block不够了/buffer_head不够了(两者相交)
+		h->b_dev = 0;										// 使用该缓冲块的设备号
+		h->b_dirt = 0;										// 脏标志,缓冲修改标志
+		h->b_count = 0;										// 缓冲块引用计数
+		h->b_lock = 0;										// 缓冲块锁定标志
+		h->b_uptodate = 0;									// 缓冲块更新标志(或称有效标志)
+		h->b_wait = NULL;									// 指向等待该缓冲块解锁的进程。
+		h->b_next = NULL;									// 指向具有相同hash值的下一个缓冲头。
+		h->b_prev = NULL;									// 指向具有相同hash值的前一个缓冲头。
+		h->b_data = (char *) b;								// 指向对应缓冲块数据块(1024字节)。
+		h->b_prev_free = h-1;								// 指向链表中前一项。
+		h->b_next_free = h+1;								// 指向链表中下一项。
 		h++;
 		NR_BUFFERS++;
-		if (b == (void *) 0x100000)
-			b = (void *) 0xA0000;
+		if (b == (void *) 0x100000)							// 若b递减到等于1MB,则跳过384KB
+			b = (void *) 0xA0000;							// 让b指向640KB
 	}
-	h--;
-	free_list = start_buffer;
-	free_list->b_prev_free = h;
-	h->b_next_free = free_list;
+
+	h--;									//让h指向最后一个有效缓冲块头。				
+	free_list = start_buffer;  				//让空闲链表头指向头一个缓冲块。
+	free_list->b_prev_free = h; 			//链表头的b_prev_free指向前一项（即最后一项）。
+	h->b_next_free = free_list; 			//h的下一项指针指向第一项，形成一个环链.
+
 	for (i=0;i<NR_HASH;i++)
 		hash_table[i]=NULL;
 }	
+
+
+
+
+
