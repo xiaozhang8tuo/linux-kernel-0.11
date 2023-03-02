f7ac17063481da9bf3b5e1041a62649dd0c6ff41
zyx <874279750@qq.com>
2023-03-02 05:17:46 -0800

    .

--- kernel/chr_drv/tty_io.c
+++ kernel/chr_drv/tty_io.c
@@ -7,46 +7,73 @@
 /*
  * 'tty_io.c' gives an orthogonal feeling to tty's, be they consoles
  * or rs-channels. It also implements echoing, cooked mode etc.
+ * tty_io.c给tty终端一种非相关的感觉，不管它们是控制台还是串行终端。
+ * 该程序同样实现了回显、规范（熟）模式等。
  *
  * Kill-line thanks to John T Kohl.
  */
 #include <ctype.h>
-#include <errno.h>
-#include <signal.h>
+#include <errno.h>		// 
+#include <signal.h>		// 信号头文件,定义信号符号常量,信号结构及其操作函数原型
 
-#define ALRMMASK (1<<(SIGALRM-1))
-#define KILLMASK (1<<(SIGKILL-1))
-#define INTMASK (1<<(SIGINT-1))
-#define QUITMASK (1<<(SIGQUIT-1))
-#define TSTPMASK (1<<(SIGTSTP-1))
+#define ALRMMASK (1<<(SIGALRM-1))		// 警告(alarm)信号屏蔽位
+#define KILLMASK (1<<(SIGKILL-1))		// 终止(kill)信号屏蔽位
+#define INTMASK (1<<(SIGINT-1))			// 键盘中断(int)信号屏蔽位
+#define QUITMASK (1<<(SIGQUIT-1))		// 键盘退出(quit)信号屏蔽位
+#define TSTPMASK (1<<(SIGTSTP-1))		// tty发出的停止进程(tty stop)信号屏蔽位
 
 #include <linux/sched.h>
 #include <linux/tty.h>
 #include <asm/segment.h>
 #include <asm/system.h>
 
-#define _L_FLAG(tty,f)	((tty)->termios.c_lflag & f)
-#define _I_FLAG(tty,f)	((tty)->termios.c_iflag & f)
-#define _O_FLAG(tty,f)	((tty)->termios.c_oflag & f)
+// 获取termios结构中三个模式标志集之一，或者用于判断一个标志集是否有置位标志
+#define _L_FLAG(tty,f)	((tty)->termios.c_lflag & f)// 本地模式标志
+#define _I_FLAG(tty,f)	((tty)->termios.c_iflag & f)// 输入模式标志
+#define _O_FLAG(tty,f)	((tty)->termios.c_oflag & f)// 输出模式标志
 
-#define L_CANON(tty)	_L_FLAG((tty),ICANON)
-#define L_ISIG(tty)	_L_FLAG((tty),ISIG)
-#define L_ECHO(tty)	_L_FLAG((tty),ECHO)
-#define L_ECHOE(tty)	_L_FLAG((tty),ECHOE)
-#define L_ECHOK(tty)	_L_FLAG((tty),ECHOK)
-#define L_ECHOCTL(tty)	_L_FLAG((tty),ECHOCTL)
-#define L_ECHOKE(tty)	_L_FLAG((tty),ECHOKE)
+// 取termios结构终端特殊（本地）模式标志集中的一个标志
+#define L_CANON(tty)	_L_FLAG((tty),ICANON)	// 取规范模式标志
+#define L_ISIG(tty)	_L_FLAG((tty),ISIG)			// 取信号标志
+#define L_ECHO(tty)	_L_FLAG((tty),ECHO)			// 取回显字符标志
+#define L_ECHOE(tty)	_L_FLAG((tty),ECHOE)	// 规范模式时取回显擦除标志
+#define L_ECHOK(tty)	_L_FLAG((tty),ECHOK)	// 规范模式时取kill擦除当前标志
+#define L_ECHOCTL(tty)	_L_FLAG((tty),ECHOCTL)	// 取回显控制字符标志
+#define L_ECHOKE(tty)	_L_FLAG((tty),ECHOKE)	// 规范模式时取kill擦除行并回显标志
 
-#define I_UCLC(tty)	_I_FLAG((tty),IUCLC)
-#define I_NLCR(tty)	_I_FLAG((tty),INLCR)
-#define I_CRNL(tty)	_I_FLAG((tty),ICRNL)
-#define I_NOCR(tty)	_I_FLAG((tty),IGNCR)
+// 取termios结构输入模式标志集中的一个标志
+#define I_UCLC(tty)	_I_FLAG((tty),IUCLC)		// 取大写到小写转换标志
+#define I_NLCR(tty)	_I_FLAG((tty),INLCR)		// 取换行符NL转回车符CR标志
+#define I_CRNL(tty)	_I_FLAG((tty),ICRNL)		// 取回车符CR转换行符NL标志
+#define I_NOCR(tty)	_I_FLAG((tty),IGNCR)		// 取忽略回车符CR标志
 
-#define O_POST(tty)	_O_FLAG((tty),OPOST)
-#define O_NLCR(tty)	_O_FLAG((tty),ONLCR)
-#define O_CRNL(tty)	_O_FLAG((tty),OCRNL)
-#define O_NLRET(tty)	_O_FLAG((tty),ONLRET)
-#define O_LCUC(tty)	_O_FLAG((tty),OLCUC)
+// 取termios结构输出模式标志集中的一个标志
+#define O_POST(tty)	_O_FLAG((tty),OPOST)		// 取执行输出处理标志
+#define O_NLCR(tty)	_O_FLAG((tty),ONLCR)		// 取换行符NL转回车换行符CR-NL标志
+#define O_CRNL(tty)	_O_FLAG((tty),OCRNL)		// 取回车符CR转换行符NL标志
+#define O_NLRET(tty)	_O_FLAG((tty),ONLRET)	// 取换行符L执行回车功能的标志
+#define O_LCUC(tty)	_O_FLAG((tty),OLCUC)		// 取小写转大写字符标志
+
+
+// tty数据结构
+// struct tty_struct {
+// 	struct termios termios;					//终端io属性和控制字符数据结构
+// 	int pgrp;								//所属进程组
+// 	int stopped;							//停止标志
+// 	void (*write)(struct tty_struct * tty);	//tty写函数指针
+// 	struct tty_queue read_q;				//tty读队列
+// 	struct tty_queue write_q;				//tty写队列
+// 	struct tty_queue secondary;				//tty辅助队列(存放规范模式字符序列)// 规范模式队列
+// };
+
+// struct termios {
+// 	unsigned long c_iflag;		/* input mode flags */
+// 	unsigned long c_oflag;		/* output mode flags */
+// 	unsigned long c_cflag;		/* control mode flags */
+// 	unsigned long c_lflag;		/* local mode flags */
+// 	unsigned char c_line;		/* line discipline */
+// 	unsigned char c_cc[NCCS];	/* control characters */
+// };
 
 struct tty_struct tty_table[] = {
 	{
@@ -96,22 +123,28 @@ struct tty_struct tty_table[] = {
  * you can implement pseudo-tty's or something by changing
  * them. Currently not done.
  */
+// tty读写缓冲队列结构地址表。供rs_io.s汇编程序使用，用于取得读写缓冲队列地址
 struct tty_queue * table_list[]={
 	&tty_table[0].read_q, &tty_table[0].write_q,
 	&tty_table[1].read_q, &tty_table[1].write_q,
 	&tty_table[2].read_q, &tty_table[2].write_q
 	};
-
+// tty中断初始化函数
+// 初始化串口终端和控制台终端
 void tty_init(void)
 {
-	rs_init();
-	con_init();
+	rs_init();	// serial.c
+	con_init();	// console.c
 }
-
+// tty键盘中断(^C)处理函数
+// 向tty结构中指明的（前台）进程组中所有进程发送指定信号mask,通常该信号是SIGINT
+// 参数：tty--指定终瑞的tty结构指针：mask-信号屏蔽位。
 void tty_intr(struct tty_struct * tty, int mask)
 {
 	int i;
 
+	// 首先检查终端进程组号。如果tty所属进程组号小于等于0，则退出。因为当pgrp=0时，
+	// 表明进程是初始进程init,它没有控制终端，因此不应该会发出中断字符。
 	if (tty->pgrp <= 0)
 		return;
 	for (i=0;i<NR_TASKS;i++)
@@ -119,16 +152,26 @@ void tty_intr(struct tty_struct * tty, int mask)
 			task[i]->signal |= mask;
 }
 
+// 如果队列缓冲区空则让进程进入可中断睡眠状态。
+// 参数：queue--指定队列的指针。
+// 进程在取队列缓冲区中字符之前需要调用此函数加以验证
 static void sleep_if_empty(struct tty_queue * queue)
 {
+	// 若当前进程没有信号要处理，并且指定的队列缓冲区空，则让进程进入可中断睡眠状态，并
+	// 让队列的进程等待指针指向该进程。
 	cli();
 	while (!current->signal && EMPTY(*queue))
 		interruptible_sleep_on(&queue->proc_list);
 	sti();
 }
 
+// 若队列缓冲区满则让进程进入可中断的睡眠状态
+// 参数：queue--指定队列的指针
+// 进程在往队列缓冲区中写入字符之前需要调用此函数判断队列情况
 static void sleep_if_full(struct tty_queue * queue)
 {
+	// 如果队列缓冲区不满则返回退出。否则若进程没有信号需要处理，并且队列缓冲区中空闲剩
+	// 余区长度<128，则让进程进入可中断睡眠状态，并让该队列的进程等待指针指向该进程。
 	if (!FULL(*queue))
 		return;
 	cli();
@@ -137,17 +180,30 @@ static void sleep_if_full(struct tty_queue * queue)
 	sti();
 }
 
+// 等待按键
+// 如果控制台读队列缓冲区空，则让进程进入可中断睡眠状态
 void wait_for_keypress(void)
 {
 	sleep_if_empty(&tty_table[0].secondary);
 }
 
+// 复制成规范模式字符序列
+// 根据终端termios结构中设置的各种标志，将指定tty终端队列缓冲区中的字符复制转换成
+// 规范模式（熟模式）字符并存放在辅助队列（规范模式队列）中。
+// 参数：tty-指定终端的tty结构指针。
 void copy_to_cooked(struct tty_struct * tty)
 {
 	signed char c;
 
+	// 如果tty的读队列缓冲区不空并且辅助队列缓冲区不满，则循环读取读队列缓冲区中的字符
+	// 转换成规范摸式后放入辅助队列(secondary)缓冲区中，直到读队列缀冲区空或者辅助队
+	// 列满为止。在循环体内，程序首先从读队列缓冲区尾指针处取一字符，并把尾指针前移一个
+	// 字符位置。然后根据终端termios中输入模式标志集中设置的标志对字符进行处理
 	while (!EMPTY(tty->read_q) && !FULL(tty->secondary)) {
 		GETCH(tty->read_q,c);
+		// 如果该字符是回车符CR(13),那么若回车转换行标志CRNL置位，则将字符转换为换行符
+		// NL(10)。否则如果忽略回车标志NOCR置位，则忽咯该字符，维续处理其他字符。如果字
+		// 符是换行符NL(10),并且换行转回车标志NLCR置位，则将其转换为回车符CR(13)
 		if (c==13)
 			if (I_CRNL(tty))
 				c=10;
@@ -156,17 +212,26 @@ void copy_to_cooked(struct tty_struct * tty)
 			else ;
 		else if (c==10 && I_NLCR(tty))
 			c=13;
+		// 如果大写转小写标志CLC置位，则将该字符转换为小写字符
 		if (I_UCLC(tty))
 			c=tolower(c);
+		// 如果本地摸式标志集中规范模式标志CANON已置位，则对读取的字符进行以下处理。首先：
+		// 如果该字符是键盘终止控制字符KILL(^U),则对已输入的当前行执行删除处理。删除一行字
+		// 符的循环过程如下：如果y辅助队列不空，并且取出的辅助队列中最后一个字符不是换行
+		// 符NL(10),并且该字符不是文件结束字符(^D),则循环执行下列代码：
+		// 如果本地回显标志ECHO置位，那么：若字符是控制字符（值<32），则往tty写队列中放
+		// 入擦除控制字符ERASE(^H)。然后再放入一个擦除字符ERASE,并且调用该tLy写函数，把
+		// 写队列中的所有字符输出到终端屏幕上。另外，因为控制字符在放入写队列时需要用2个字
+		// 节表示（例如^V),因此要求特别对控制字符多放入一个ERASE。最后将tty辅助队列头指针后退1字节。
 		if (L_CANON(tty)) {
 			if (c==KILL_CHAR(tty)) {
 				/* deal with killing the input line */
 				while(!(EMPTY(tty->secondary) ||
-				        (c=LAST(tty->secondary))==10 ||
+				        (c=LAST(tty->secondary))==10 ||//删除一行即删除到只剩换行符
 				        c==EOF_CHAR(tty))) {
 					if (L_ECHO(tty)) {
 						if (c<32)
-							PUTCH(127,tty->write_q);
+							PUTCH(127,tty->write_q);//控制字符要删除两个字节
 						PUTCH(127,tty->write_q);
 						tty->write(tty);
 					}
